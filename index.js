89fac5e3e960f19ae87f7d5c68bc7d08ac7a0989
Michał Budzyński <michal@virtualdesign.pl>
2016-09-18 03:34:00 +0100

    :roller_coaster: basic promise

---
+++ index.js
@@ -0,0 +1,12 @@
+/* exported LURKER */
+'use strict';
+
+var LURKER = function(episodeObject, cb) {
+  // var title = episodeObject.title;
+  // var season = episodeObject.season;
+  // var episode = episodeObject.episode;
+
+  return new Promise(function(resolve, reject) {
+
+  });
+};
